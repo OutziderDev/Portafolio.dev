104b505d4327cadfa15cd2e80b21bb979557620d
Luis <luisbarrios0110@gmail.com>
2025-02-05 01:25:15 -0500

    Modificando la descripcion del Hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,28 @@ export default {
       colors:{
         secundary: "#A00",
         primary: "#111111"
+      },
+      keyframes:{
+        fadeIn:{
+          '0%':{
+            opacity:0
+          },
+          '100%':{
+            opacity:1
+          }
+        },
+        fadeOut:{
+          '0':{
+            opacity:100
+          },
+          '100':{
+            opacity:0
+          }
+        }
+      },
+      animation:{
+        fadeIn: 'fadeIn 0.5s ease-in',
+        fadeOut: 'fadeOut 0.5s ease-out'
       }
     },
 	},
