48ed1a33b1d74206d67fe8ad29fafe4246b4b4d4
Luis <luisbarrios0110@gmail.com>
2025-03-27 18:43:21 -0500

    Generando las constantes de informacion del proyecto

---
+++ src/consts/estudios.ts
@@ -0,0 +1,30 @@
+export const estudios = [
+    {
+      "titulo": "Bootcamp - FullStackOpen",
+      "subtitulo": "Universidad de Helsinki",
+      "fecha": "2024 - 2025",
+      "pos": "justify-start",
+      "descrip": "Bootcamp de desarrollo FullStack que cubre tecnologías como JavaScript, React, Node.js, MongoDB, entre otras. Abarca la construcción de aplicaciones escalables, testing, control de versiones, despliegue y buenas prácticas en el desarrollo web moderno."
+    },
+    {
+      "titulo": "Curso de Figma para Devs",
+      "subtitulo": "Midudev Academy",
+      "fecha": "2025",
+      "pos": "justify-end",
+      "descrip": "Es un curso de Figma enfocado en desarrolladores, donde se aprende a diseñar interfaces, colaborar con equipos de diseño y optimizar el flujo de trabajo. Ideal para mejorar la comunicación entre diseño y desarrollo en proyectos web."
+    },
+    {
+      "titulo": "Curso de MySQL y PHP",
+      "subtitulo": "Online",
+      "fecha": "2022",
+      "pos": "justify-start",
+      "descrip": "Curso online de desarrollo web con PHP y MySQL. Se enseña desde los fundamentos hasta la creación de aplicaciones avanzadas con POO, MVC y más. También se abarcan frameworks y la gestión de contenidos con WordPress."
+    },
+    {
+      "titulo": "Ing. Desarrollo de Software",
+      "subtitulo": "Universidad Tecnológica de Panamá",
+      "fecha": "2016 - 2019",
+      "pos": "justify-end",
+      "descrip": "Bajo la Facultad de Ingeniería de Sistemas Computacionales, culminé mis estudios, adquiriendo conocimientos en lógica de programación, desarrollo de aplicaciones en diversos lenguajes, bases de datos y otras tecnologías."
+    }
+  ]
---
+++ src/consts/proyectos.ts
@@ -0,0 +1,67 @@
+export const proyectos = [
+    {
+      "titulo": "Portafolio Web",
+      "slug": "portafolio-web",
+      "type": "WEB",
+      "estado": true,
+      "fecha": 2025,
+      "demoUrl": "https://github.com/OutziderDev/Portafolio.dev",
+      "gitUrl": "https://github.com/OutziderDev/Portafolio.dev",
+      "descripcion": "Portafolio digital donde presento mi experiencia, habilidades y proyectos de forma profesional. También incluyo información sobre mí, enlaces a mis trabajos y una muestra de lo que puedo aportar.",
+      "image": "portafolio.webp",
+      "stack": ["Aastro","JavaScript","Tailwind","Github"],
+      "disable": false
+    },
+    {
+      "titulo": "C.G Properties",
+      "slug": "cg-properties",
+      "type": "WEB",
+      "estado": true,
+      "fecha": 2025,
+      "demoUrl": "cg-properties-project-one.vercel.app",
+      "gitUrl": "https://github.com/OutziderDev/CGProperties-Project",
+      "descripcion": "Proyecto profesional para un cliente quien deseaba tener su web para ofertar sus productos y servicios manteniendo el control de su contenido en todo momento.",
+      "image": "c.g.p.webp",
+      "stack": ["Cloudinary","Strapi","Aastro","Tailwind","Github"],
+      "disable": false
+    },
+    {
+      "titulo": "La Velada del Año",
+      "slug": "la-velada-oficial",
+      "type": "Landing Page",
+      "estado": false,
+      "fecha": 2025,
+      "demoUrl": "https://www.infolavelada.com/",
+      "gitUrl": "https://github.com/midudev/la-velada-web-oficial",
+      "descripcion": "Desarrollo de la Landing Page de  La velada del año de Ibai llanos de Codigo Abierto",
+      "image": "",
+      "stack": ["Aastro","Tailwind","Github"],
+      "disable": false
+    },
+    {
+      "titulo": "Blog App - FSO",
+      "slug": "blog-app-fso",
+      "type": "SPA",
+      "estado": false,
+      "fecha": 2024,
+      "demoUrl": "https://github.com/OutziderDev/FullStack/tree/main/Part5/BlogListFrontend",
+      "gitUrl": "https://github.com/OutziderDev/FullStack/tree/main/Part5/BlogListFrontend",
+      "descripcion": "Aplicación web full-stack que permite a los usuarios registrar, gestionar y compartir sus blogs de forma sencilla. Desarrollada como parte del curso Fso de la Universidad de Helsinki, utilizando tecnologías modernas de frontend y backend",
+      "image": "blogApp.webp",
+      "stack": ["JavaScript","ReactJs","Github","Tailwind","Express"],
+      "disable": true
+    },
+    {
+      "titulo": "Country Data - FSO",
+      "slug": "country-data-fso",
+      "type": "WEB",
+      "estado": false,
+      "fecha": 2024,
+      "demoUrl": "https://github.com/OutziderDev/FullStack/tree/main/Part7/Countries-Hook",
+      "gitUrl": "https://github.com/OutziderDev/FullStack/tree/main/Part7/Countries-Hook",
+      "descripcion": "Aplicación web que permite a los usuarios consultar datos básicos y curiosos sobre diversos países, como su población, idiomas y más. Consume una API  para obtener la información en tiempo real. Desarrollada como parte del curso de Fullstack Open ",
+      "image": "weaterApp.webp",
+      "stack": ["ReactJs","JavaScript","Github","Tailwind","TanStack"],
+      "disable": true
+    }
+  ]
